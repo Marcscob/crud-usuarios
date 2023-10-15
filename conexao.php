ea6bcf1f39402503d486467af531cb6e92fa5819
Marcscob <59457548+Marcscob@users.noreply.github.com>
2023-10-15 19:40:26 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+  
+$host = "localhost";
+$user="root";
+$pass="";
+$db= "crud_users";
+
+$mysqli=new mysqli($host, $user, $pass, $db);
+if($mysqli -> connect_errno) {
+    die("Falha na conexão com o bd");
+}
+
+?>
