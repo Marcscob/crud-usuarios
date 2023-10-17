f5d1fdbd586bbd3dce2ba3d2e02e15caf8cabfa8
Marcscob <marcscob@hotmail.com>
2023-10-17 10:23:50 -0300

    modificação importante

--- conexao.php
+++ conexao.php
@@ -1,14 +1,22 @@
-
 <?php
-  
-$host = "localhost";
-$user="root";
-$pass="";
-$db= "crud_users";
 
-$mysqli=new mysqli($host, $user, $pass, $db);
-if($mysqli -> connect_errno) {
-    die("Falha na conexão com o bd");
+$host = "localhost";
+$db = "crud_clientes";
+$user = "root";
+$pass = "";
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+if($mysqli->connect_errno) {
+    die("Falha na conexão com o banco de dados");
 }
 
-?>
+function formatar_data($data){
+    return implode('/', array_reverse(explode('-', $data)));
+}
+
+function formatar_telefone($telefone){
+    $ddd = substr ($telefone, 0, 2);
+    $parte1 = substr ($telefone, 2, 5);
+    $parte2 = substr ($telefone, 7);
+    return "($ddd) $parte1-$parte2";
+}
